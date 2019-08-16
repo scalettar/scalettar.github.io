7f440aef285e6a1b119538820312a7c130b7d25f
Daniel Scalettar <scalettar+github@gmail.com>
2019-08-15 18:48:12 -0700

    Add favicon. Tweak maintenance message.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="maintenance">Down for maintenance...</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
