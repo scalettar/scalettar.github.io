eb6b03fea17a67fa551d4efb2940c81c23acdb84
Daniel Scalettar <scalettar+github@gmail.com>
2019-08-15 19:18:21 -0700

    Updates

---
+++ precache-manifest.081e3377e647754322c23882adf5761f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7888df2b59767cb29379a0be99a83140",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a559a4e8c711ccdd2d9",
+    "url": "/static/css/main.53ae568e.chunk.css"
+  },
+  {
+    "revision": "3254d1ba73b5f22acc33",
+    "url": "/static/js/2.343f22e8.chunk.js"
+  },
+  {
+    "revision": "0a559a4e8c711ccdd2d9",
+    "url": "/static/js/main.648ca08b.chunk.js"
+  },
+  {
+    "revision": "fbbc75e3050b6a5430fb",
+    "url": "/static/js/runtime~main.d8d1271d.js"
+  }
+]);
